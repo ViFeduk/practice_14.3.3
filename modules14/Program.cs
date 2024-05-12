b2ec129c50836b9a105e72ac5b2115344afe0657
ViFeduk <viktor2005fedchenkov@mail.ru>
2024-05-12 19:26:14 +0300

    Добавьте файлы проекта.

---
+++ modules14/Program.cs
@@ -0,0 +1,65 @@
+﻿namespace modules14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var phoneBook = new List<Contact>();
+
+            // добавляем контакты
+            phoneBook.Add(new Contact("Игорь", "Николаев", 79990000001, "igor@example.com"));
+            phoneBook.Add(new Contact("Сергей", "Довлатов", 79990000010, "serge@example.com"));
+            phoneBook.Add(new Contact("Анатолий", "Карпов", 79990000011, "anatoly@example.com"));
+            phoneBook.Add(new Contact("Валерий", "Леонтьев", 79990000012, "valera@example.com"));
+            phoneBook.Add(new Contact("Сергей", "Брин", 799900000013, "serg@example.com"));
+            phoneBook.Add(new Contact("Иннокентий", "Смоктуновский", 799900000013, "innokentii@example.com"));
+            
+            while (true)
+            {
+               
+                var input = Console.ReadKey().KeyChar;
+
+               
+                var parsed = Int32.TryParse(input.ToString(), out int pageNumber);
+
+                Console.Clear();
+                if (!parsed || pageNumber < 1 || pageNumber > 3)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Страницы не существует");
+                }
+                
+                else
+                {
+                    
+                    var pageContent = phoneBook.OrderBy(phone => phone.Name).ThenBy(phone => phone.LastName).Skip((pageNumber - 1) * 2).Take(2);
+                    Console.WriteLine();
+
+                    
+                    foreach (var entry in pageContent)
+                        Console.WriteLine(entry.Name + " " + entry.LastName + ": " + entry.PhoneNumber);
+
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+
+    
+
+    public class Contact // модель класса
+    {
+        public Contact(string name, string lastName, long phoneNumber, String email) // метод-конструктор
+        {
+            Name = name;
+            LastName = lastName;
+            PhoneNumber = phoneNumber;
+            Email = email;
+        }
+
+        public String Name { get; }
+        public String LastName { get; }
+        public long PhoneNumber { get; }
+        public String Email { get; }
+    }
+}
